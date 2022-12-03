d188e0fbda330a9d7c37d6fac225093e8334b8dd
Erick <erickvaldez2001@gmail.com>
2022-12-03 01:19:58 -0600

    CON-36 (Flask) Conexion del boton -si- con la pagina web EDG . EV

--- routes/routesWeb.py
+++ routes/routesWeb.py
@@ -4,9 +4,17 @@ from flask import Blueprint, flash, render_template, request, redirect, url_for
 ## Imports internos
 
 ### Blueprint ###
-web = Blueprint('WebRoutes',__name__)
+web = Blueprint('webRoutes',__name__)
 
 ## Flask web routes ##
 @web.route('/')
 def index():
     return render_template('advertencia.html')
+
+@web.route('/edg')
+def adviceEDG():
+    try:
+        return render_template('mod/pagEDG.html')
+    except:
+        flash('No se pudieron obtener los alumnos')
+
