b1b1fe1777196dd80262f040f1c62182026cfa0e
Erick <erickvaldez2001@gmail.com>
2023-05-09 16:47:33 -0600

    CON-46 (Flask) Creacion del modelo Flask -users- . EV

--- models/comment.py
+++ models/comment.py
@@ -3,12 +3,14 @@ from utils.db import db
 class Comentarios(db.Model):
     id_comentario = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100))
-    telefono = db.Column(db.String(12))
     email = db.Column(db.String(100))
     mensaje = db.Column(db.String(255))
+    estado = db.Column(db.String(20))
 
-    def __init__(self,nombre,telefono,email,mensaje):
+    def __init__(self,nombre,email,mensaje, estado):
         self.nombre = nombre
-        self.telefono = telefono
         self.email = email
         self.mensaje = mensaje
+        self.estado = estado
+
+
--- routes/routesWeb.py
+++ routes/routesWeb.py
@@ -1,5 +1,7 @@
 ## Imports externos
 from flask import Blueprint, flash, render_template, request, redirect, url_for
+from models.comment import Comentarios
+from utils.db import db
 
 ## Imports internos
 
@@ -13,17 +15,95 @@ def index():
     return render_template('advertencia.html')
 
 @web.route('/edg')
-def adviceEDG():
+def adviceEDG(): 
     try:
-        return render_template('mod/pagEDG.html')
+        return redirect(url_for('webRoutes.adminEDG'))
     except:
-        flash('No se pudieron obtener los alumnos')
+        flash('No se pudo obtener la pagina web')
 
 @web.route('/color')
 def adviceColor():
     try:
-        return render_template('mod/pagCol.html')
+        return redirect(url_for('webRoutes.adminColor'))
     except:
-        flash('No se pudieron obtener los alumnos')
+        flash('No se pudo obtener la pagina web')
 
 
+@web.route('/PageColor', methods=['GET', 'POST'])
+def adminColor():
+    if request.method == 'POST':
+        nombre = request.form['txtNombre']
+        email = request.form['txtEmail']
+        mensaje = request.form['txtMensaje']
+        value = {'scroll': "true"}
+        if 'cBox' in request.form:
+            check = True
+        else:
+            check = False
+
+        if nombre == ""  or email == "" or mensaje == "":
+            flash('Rellena todo lo del formulario')
+            return render_template('mod/pagCol.html',value=value)
+        else:
+            if check == False:
+                flash('Acepta los Terminos y condiciones y Politicas de Privacidad Primero')
+                return render_template('mod/pagCol.html',value=value)
+            else:
+                try:
+                    newComment = Comentarios(nombre,email,mensaje,"pendiente")
+                    db.session.add(newComment)
+                    db.session.commit()
+                    flash('El comentario se envio correctamente, espera tu respuesta por tu correo :)')
+                    return render_template('mod/pagCol.html', value=value)
+                except:
+                    flash('No se pudo enviar el comentario, lo siento :(')
+                    return render_template('mod/pagCol.html', value=value)
+    else:
+       value = {'scroll': "false"}
+       return render_template('mod/pagCol.html', value=value)
+
+@web.route('/PageEDG', methods=['GET', 'POST'])
+def adminEDG():
+    if request.method == 'POST':
+        nombre = request.form['txtNombre']
+        email = request.form['txtEmail']
+        mensaje = request.form['txtMensaje']
+        value = {'scroll': "true"}
+        if 'cBox' in request.form:
+            check = True
+        else:
+            check = False
+
+        if nombre == "" or email == "" or mensaje == "":
+            flash('Rellena todo lo del formulario')
+            return render_template('mod/pagEDG.html', value=value)
+        else:
+            if check == False:
+                flash('Acepta los Terminos y condiciones y Politicas de Privacidad Primero')
+                return render_template('mod/pagEDG.html', value=value)
+            else:
+                try:
+                    newComment = Comentarios(nombre,email,mensaje,"inatendido")
+                    db.session.add(newComment)
+                    db.session.commit()
+                    flash('El comentario se envio correctamente, espera tu respuesta por tu correo :)')
+                    return render_template('mod/pagEDG.html', value=value)
+                except:
+                    flash('No se pudo enviar el comentario, lo siento :(')
+                    return render_template('mod/pagEDG.html', value=value)
+    else:
+       value = {'scroll': "false"}
+       return render_template('mod/pagEDG.html', value=value)
+    
+
+@web.route('/TCPPC')
+def TCPPC():
+    return render_template('mod/terCol.html')
+
+@web.route('/TCPPE')
+def TCPPE():
+    return render_template('mod/terEDG.html')
+
+@web.route('/c')
+def recEDG():
+    return render_template('mod/pagEDG.html')
